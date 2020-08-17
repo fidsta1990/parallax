7d8071eeed3efd2e969ca6f5161f0c4330946e5a
Fidel Collin <fidelcollin@icloud.com>
2020-08-17 13:32:35 +0100

    add menu toggle to script and markup files

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+// Show Date
 const date = document.querySelector(".date");
-
 date.innerHTML = new Date().getFullYear();
+
+// Toggle Sidebar
+const sidebar = document.getElementById("sidebar");
+const menu = document.getElementById("menu-toggle");
+const closed = document.getElementById("close-btn");
+
+menu.addEventListener("click", () => {
+  sidebar.classList.add("show-bar");
+});
+
+closed.addEventListener("click", () => {
+  sidebar.classList.remove("show-bar");
+});
